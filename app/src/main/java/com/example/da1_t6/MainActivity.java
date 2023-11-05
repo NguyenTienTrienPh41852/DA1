5355dfba207215096c471b92a24ca6865fdece3a
NguyenTrien <trienntph41852@fpt.edu.vn>
2023-11-06 00:28:07 +0700

    lan3

--- app/src/main/java/com/example/da1_t6/MainActivity.java
+++ app/src/main/java/com/example/da1_t6/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Integer integer = new Integer(2);
+        String val = new String("");
     }
 }

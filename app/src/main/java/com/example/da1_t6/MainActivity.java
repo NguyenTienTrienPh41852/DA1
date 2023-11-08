7472914f9a8f937075f180dceb61ea086583c51e
NguyenTrien <trienntph41852@fpt.edu.vn>
2023-11-08 19:32:03 +0700

    trien fragment

--- app/src/main/java/com/example/da1_t6/MainActivity.java
+++ app/src/main/java/com/example/da1_t6/MainActivity.java
@@ -1,16 +1,70 @@
 package com.example.da1_t6;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.BarChart;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.google.android.material.navigation.NavigationView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Integer integer = new Integer(2);
-        String val = new String("");
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+
+
+    }
+    public void relaceFrg(Fragment frg){
+        FragmentManager fg = getSupportFragmentManager();
+        fg.beginTransaction().replace(R.id.frameLayout,frg).commit();
     }
 }
---
+++ app/src/main/java/com/example/da1_t6/fragment_Thongke.java
@@ -0,0 +1,68 @@
+package com.example.da1_t6;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
+public class fragment_Thongke extends Fragment {
+
+    public fragment_Thongke() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+       View view = inflater.inflate(R.layout.fragment__thongke, container, false);
+
+        BarChart barChart = view.findViewById(R.id.barchart_chitieu);
+        ArrayList<BarEntry> visitor = new ArrayList<>();
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+
+        visitor.add(new BarEntry(2014,657));
+        visitor.add(new BarEntry(2015,687));
+        visitor.add(new BarEntry(2016,697));
+        visitor.add(new BarEntry(2017,737));
+        visitor.add(new BarEntry(2018,767));
+        visitor.add(new BarEntry(2019,857));
+        visitor.add(new BarEntry(2020,457));
+
+        BarDataSet barDataSet = new BarDataSet(visitor, "Chi tiêu");
+        barDataSet.setColor(Color.RED);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.animateY(2000);
+
+
+        return view;
+    }
+}

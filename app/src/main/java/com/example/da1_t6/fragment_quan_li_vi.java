50eac3e4261e0608ddd5126e88b4e5a52d4c62ba
DELL <nhhoa03@gmail.com>
2023-11-09 11:55:16 +0700

    fragment hoa

---
+++ app/src/main/java/com/example/da1_t6/fragment_quan_li_vi.java
@@ -0,0 +1,23 @@
+package com.example.da1_t6;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class fragment_quan_li_vi extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_quan_li_vi,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}

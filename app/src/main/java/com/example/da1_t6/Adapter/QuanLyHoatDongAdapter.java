d76a79ebc6abfaefc159e151973744b5bf155f05
DELL <nhhoa03@gmail.com>
2023-11-15 08:13:53 +0700

    adapter va update 1 it layout

--- app/src/main/java/com/example/da1_t6/Adapter/QuanLyHoatDongAdapter.java
+++ app/src/main/java/com/example/da1_t6/Adapter/QuanLyHoatDongAdapter.java
@@ -1,4 +1,72 @@
 package com.example.da1_t6.Adapter;
 
-public class QuanLyHoatDongAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.da1_t6.Model.HoatDong;
+import com.example.da1_t6.R;
+
+import java.util.List;
+
+public class QuanLyHoatDongAdapter extends RecyclerView.Adapter<QuanLyHoatDongAdapter.viewHolder>{
+    Context context;
+    List<HoatDong> listHD;
+
+    public QuanLyHoatDongAdapter(Context context, List<HoatDong> listHD) {
+        this.context = context;
+        this.listHD = listHD;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        View v = inflater.inflate(R.layout.item_thoi_gian_bieu,parent,false);
+        return new viewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+        HoatDong hoatDong = listHD.get(position);
+
+        holder.tvTenHoatDong.setText(hoatDong.getTenHoatDong());
+        holder.tvMoTa.setText("Mô tả: "+hoatDong.getMoTa());
+        holder.tvThoiGian.setText("Thời gian: "+hoatDong.getThoiGianBatDau()+" đến "+hoatDong.getThoiGianKetThuc());
+
+        if (hoatDong.getTrangThaiHoatDong() == 1){
+            holder.tvStatus.setText("Đã hoàn thành");
+            holder.tvStatus.setTextColor(Color.GREEN);
+        } else {
+            holder.tvStatus.setText("Chưa hoàn thành");
+            holder.tvStatus.setTextColor(Color.RED);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listHD.size();
+    }
+
+    public static class viewHolder extends RecyclerView.ViewHolder {
+        TextView tvTenHoatDong, tvMoTa, tvThoiGian, tvStatus;
+        ImageView imgDel;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTenHoatDong = itemView.findViewById(R.id.tv_ten_hoat_dong);
+            tvMoTa = itemView.findViewById(R.id.tv_mo_ta_hoat_dong);
+            tvThoiGian = itemView.findViewById(R.id.tv_thoi_gian_hoat_dong);
+            tvStatus = itemView.findViewById(R.id.tv_status_hoat_dong);
+            imgDel = itemView.findViewById(R.id.img_delete);
+        }
+    }
 }
--- app/src/main/java/com/example/da1_t6/Adapter/QuanLyViAdapter.java
+++ app/src/main/java/com/example/da1_t6/Adapter/QuanLyViAdapter.java
@@ -1,4 +1,61 @@
 package com.example.da1_t6.Adapter;
 
-public class QuanLyViAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.da1_t6.Model.ViTien;
+import com.example.da1_t6.R;
+
+import java.util.List;
+
+public class QuanLyViAdapter extends RecyclerView.Adapter<QuanLyViAdapter.viewHolder>{
+    Context context;
+    List<ViTien> listVi;
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        View v = inflater.inflate(R.layout.item_chon_vi,parent,false);
+
+        return new viewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        ViTien viTien = listVi.get(position);
+
+        holder.tvTenVi.setText(viTien.getTenVi());
+        holder.tvSoTien.setText(viTien.getSoDuBanDau()+"");
+
+        holder.imgDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listVi.size();
+    }
+
+    public static class viewHolder extends RecyclerView.ViewHolder {
+        TextView tvTenVi, tvSoTien;
+        ImageView imgIcon, imgDel;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgDel = itemView.findViewById(R.id.img_delete);
+            tvTenVi = itemView.findViewById(R.id.tv_ten_vi);
+            tvSoTien = itemView.findViewById(R.id.tv_so_tien);
+        }
+    }
 }

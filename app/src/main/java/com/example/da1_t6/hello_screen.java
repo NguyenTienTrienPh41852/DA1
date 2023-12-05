37dc352cd51440e01d17e96aaf1faac890635d68
NguyenTrien <trienntph41852@fpt.edu.vn>
2023-12-05 13:13:43 +0700

    manhinhchao l2

--- app/src/main/java/com/example/da1_t6/hello_screen.java
+++ app/src/main/java/com/example/da1_t6/hello_screen.java
@@ -9,37 +9,73 @@ import android.os.Handler;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class hello_screen extends AppCompatActivity {
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
 
+public class hello_screen extends AppCompatActivity {
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_screen);
-        VideoView videoView = findViewById(R.id.video);
+        playerView = findViewById(R.id.playerView);
 
-        // Đường dẫn của video bạn muốn hiển thị
+        // Đường dẫn của video trong thư mục raw
         Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videologo);
 
-        // Thiết lập MediaController để điều khiển video (play, pause, seek, ...)
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
+        // Tạo một ExoPlayer
+        player = new SimpleExoPlayer.Builder(this).build();
 
-        // Đặt đường dẫn video cho VideoView
-        videoView.setVideoURI(videoUri);
+        // Set Uri cho ExoPlayer
+        player.setMediaItem(MediaItem.fromUri(videoUri));
+
+        // Kết nối ExoPlayer với PlayerView
+        playerView.setPlayer(player);
+
+        // Bắt sự kiện khi video đã sẵn sàng để phát
+        player.addListener(new ExoPlayer.EventListener() {
+            @Override
+            public void onIsPlayingChanged(boolean isPlaying) {
+                if (isPlaying) {
+                    // Sử dụng Handler để chuyển màn hình sau 5 giây
+                    handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Chuyển đến màn hình chính hoặc màn hình khác
+                            Intent intent = new Intent(hello_screen.this, DangKy.class);
+                            startActivity(intent);
+
+                            // Đóng màn hình hiện tại
+                            finish();
+                        }
+                    }, 2500); // 5000 milliseconds = 5 giây
+                }
+            }
+        });
+
+        // Bắt sự kiện khi video phát xong
 
         // Bắt đầu phát video
-        videoView.start();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Chuyển đến màn hình chính hoặc màn hình khác
-                Intent intent = new Intent(hello_screen.this, DangKy.class);
-                startActivity(intent);
-                // Đóng màn hình hiện tại
-                finish();
-            }
-        }, 2500); // 1000 milliseconds = 1 giây
+        player.prepare();
+        player.setPlayWhenReady(true);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Giải phóng ExoPlayer khi không sử dụng nữa
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+        // Hủy bỏ handler để tránh rò rỉ bộ nhớ
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
     }
 }
